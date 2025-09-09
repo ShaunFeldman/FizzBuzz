0f6f593fa806c1babab887015b475d079f4054ff
ShaunFeldman <shaunfeldman1608@gmail.com>
2025-09-09 13:59:35 -0400

    updated to be for multiples of 1 or 2 of any 2 values

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -1,4 +1,19 @@
 package lab1;
 
 public class Multiples {
+    public static void main(String[] args) {
+        int num = 1000;
+        System.out.println((multiples(num, 3, 5)));
+        System.out.println((multiples(10, 3, 5)));
+    }
+
+    public static int multiples(int n, int a, int b) {
+        int counter = 0;
+        for (int i = 1; i < n; i++) {  // strictly less than n
+            if (i % a == 0 || i % b == 0) {
+                counter++;
+            }
+        }
+        return counter;
+    }
 }
